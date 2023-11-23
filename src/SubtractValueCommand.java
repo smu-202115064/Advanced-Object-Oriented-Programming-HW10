936738fd70a5258b85cdd0f4188a8d9dfe3f4d92
Hepheir <hepheir@gmail.com>
2023-11-23 13:51:14 +0900

    feat(SquareValueCommand): Command의 구현체 SquareValueCommand 추가

---
+++ src/SubtractValueCommand.java
@@ -0,0 +1,14 @@
+public class SubtractValueCommand implements Command {
+    private int x;
+    private int y;
+
+    public SubtractValueCommand(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int execute() {
+        return x - y;
+    }
+}
