f1fa8c314f50f6ae90ac49cf537758275a3664e0
Hepheir <hepheir@gmail.com>
2023-11-23 13:30:30 +0900

    feat(CommandManager): CommandManager 클래스 추가

---
+++ src/CommandManager.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class CommandManager {
+    private List<Command> opList;
+
+    public CommandManager() {
+        // TODO
+    }
+
+    void addOperation(Command op) {
+        // TODO
+    }
+
+    void performOperations() {
+        // TODO
+    }
+}
